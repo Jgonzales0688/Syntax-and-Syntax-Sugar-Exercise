e613bc07cf59978a6a5a521d99f4623a83b7fae4
Jessica Gonzales <jgonzales0688@gmail.com>
2022-11-29 14:32:39 -0600

    Syntax exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -3,8 +3,15 @@
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        { //inferred typing
+          //string interpolation
+          //and the ternary operator
+
+            var answer = 5;
+            string response = (answer < 9) ? ($"{answer} is less than nine"):($"{answer} is greater than or equal to nine");
+
+            Console.WriteLine(response);
+           
         }
     }
 }
